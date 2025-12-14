037cb9ab65c226e89d8128a148026bda3b4dcc50
24rush <iordache.alin@yahoo.com>
2025-12-14 19:56:08 +0200

    Fixed redirect in app.links

--- api/strava.js
+++ api/strava.js
@@ -58,7 +58,15 @@ export default async function handler(req, res) {
     }
 
     if (!actData) {
-        const response = await fetch(strava_url);
+        const response = await fetch(strava_url, {
+            headers: {
+                'User-Agent':
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) ' +
+                    'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+                    'Chrome/120.0.0.0 Safari/537.36'
+            }
+        });
+        
         actData = extractStravaData(await response.text());
     }
 
